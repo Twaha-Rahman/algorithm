4ea06f6b0c66b725a1f95f05a83cef8917b475be
Twaha-Rahman <mahi6703890@gmail.com>
2020-04-30 12:36:07 +0600

    Solved the Optional Second Argument Sum algo

--- FCC/Arguments Optional/main.js
+++ FCC/Arguments Optional/main.js
@@ -1,12 +1,18 @@
 function addTogether(num1, num2) {
-
-  if (((typeof num1) === "number") && ((typeof num2) === "number")) {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2;
-  } else {
-    
   }
 
-  return false;
+  if (typeof num1 !== 'number' || typeof num1 !== 'number') {
+    return undefined;
+  }
+
+  if (!num2) {
+    const customFunc = (laterNum) => {
+      return addTogether(num1, laterNum);
+    };
+    return customFunc;
+  }
 }
 
-addTogether(2,3);
+addTogether(2, 3);
